4c69c11ae922127eb20b61db3f3ef709cb32d452
benjaminPla <benjaminpla.dev@gmail.com>
2021-12-06 19:28:14 -0300

    add components/SignIn.vue

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,11 +1,29 @@
 <template>
-  <input type='email' placeholder='Email...'/>
-  <input type='password' placeholder='Password...'/>
-  <button>Sign In</button>
+  <input type='email' placeholder='Email...' v-model='email'/>
+  <input type='password' placeholder='Password...' v-model='password'/>
+  <button @click.prevent='signin'>Sign In</button>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
   name: 'SignIn',
+  setup() {
+    const email = ref('');
+    const password = ref('');
+    /* eslint-disable */
+    const signin = async () => {
+      await fetch('http://localhost:3001/signin', {
+        method: 'post',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ email: email.value }),
+      })
+      .then(async (data) => console.log(await data.json()))
+      .catch((error) => console.log(error));
+    };
+    /* eslint-enable */
+    return { email, password, signin };
+  },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,12 @@
 <template>
-  <p>{{ user.msg }}</p>
+  <sign-in />
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
+import SignIn from '../components/SignIn.vue';
 
 export default {
   name: 'Home',
-  setup() {
-    const user = ref(null);
-    /* eslint-disable */
-    onMounted(async () => {
-      await fetch('http://localhost:3001/signin', {
-        method: 'post',
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify({ email: 'benjaminpla.dev@gmail.com' }),
-      })
-      .then(async (data) => user.value = await data.json())
-      .catch((error) => console.log(error));
-      });
-    /* eslint-enable */
-    return { user };
-  },
+  components: { SignIn },
 };
 </script>
