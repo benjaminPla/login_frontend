18d974e89e5043965ce965116d3cde93aee78da5
benjaminPla <benjaminpla.dev@gmail.com>
2021-12-06 19:10:54 -0300

    basic structure

---
+++ src/components/SignIn.vue
@@ -0,0 +1,11 @@
+<template>
+  <input type='email' placeholder='Email...'/>
+  <input type='password' placeholder='Password...'/>
+  <button>Sign In</button>
+</template>
+
+<script>
+export default {
+  name: 'SignIn',
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <p>{{ user.msg }}</p>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import { ref, onMounted } from 'vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  setup() {
+    const user = ref(null);
+    /* eslint-disable */
+    onMounted(async () => {
+      await fetch('http://localhost:3001/signin', {
+        method: 'post',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ email: 'benjaminpla.dev@gmail.com' }),
+      })
+      .then(async (data) => user.value = await data.json())
+      .catch((error) => console.log(error));
+      });
+    /* eslint-enable */
+    return { user };
   },
 };
 </script>
